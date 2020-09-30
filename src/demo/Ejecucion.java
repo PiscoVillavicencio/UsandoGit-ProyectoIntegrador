5b38d989e6c8f8df6eaaf8b94627f8b14f951299
PiscoVillavicencio <jorge.pisco.villavicencio@gmail.com>
2020-09-29 19:39:57 -0500

    Primer commit

---
+++ src/demo/Ejecucion.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class Ejecucion {
+	
+	public static void main(String[] args) {
+		
+		int x=1;
+		int y=2;
+		int z=3;
+		
+		
+		System.out.print(x);
+		System.out.print(y);
+		System.out.print(z);
+		
+	}
+
+}
